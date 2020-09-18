b58d31f34def89a325b90b1f6490c5d24715e3e6
xScoopy <JLeary070@gmail.com>
2020-09-17 21:36:07 -0700

    cart fully functional

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,7 @@
 import data from './data.js'
 const itemsContainer = document.getElementById('items')
 const itemList = document.getElementById('item-list')
-itemList.innerHTML = '<li>Hello World </li>';
+//itemList.innerHTML = '<li>Hello World </li>';
 const cartQty = document.getElementById('cart-qty')
 const cartTotal = document.getElementById('cart-total')
 
@@ -43,12 +43,36 @@ for (let i=0; i<data.length; ++i) {
    }))
 // declare cart array
 const cart = [ ];
+// handle on change event for update input
+itemList.onchange = function(e){
+    if(e.target && e.target.classList.contains('update')) {
+        const name = e.target.dataset.name
+        const qty = parseInt(e.target.value)
+        updateCart(name, qty)
+    }
+}
+// handle clicks on list
+itemList.onclick = function(e) {
+    // console.log("clicked list!!!!!")
+    // console.log(e.target)
+    if(e.target && e.target.classList.contains('remove')) {
+        const name = e.target.dataset.name
+        removeItem(name)
+    } else if (e.target && e.target.classList.contains('add-one')) {
+        const name = e.target.dataset.name
+        addItem(name)
+    } else if (e.target && e.target.classList.contains('remove-one')) {
+        const name = e.target.dataset.name
+        removeItem(name, 1)
+    }
+}
 
 // define function addItem
 function addItem(name, price) {
     for (let i = 0; i < cart.length; i ++) {
         if (cart[i].name === name) {
             cart[i].qty += 1;
+            showItems()
             return
         }
     }
@@ -70,7 +94,13 @@ function showItems() {
 
         const { name, price, qty } = cart[i]
 
-        itemStr += `<li>${name} $${price} x ${qty} = $${qty * price}</li>`
+        itemStr += `<li>
+        ${name} $${price} x ${qty} = $${qty * price} 
+        <button class="remove" data-name="${name}">Remove</button>
+        <button class="add-one" data-name="${name}">+</button>
+        <button class="remove-one" data-name="${name}">-</button>
+        <input class = "update" type="number" data-name="${name}">
+        </li>`
     }
     itemList.innerHTML = itemStr
 
@@ -103,22 +133,35 @@ function removeItem(name, qty = 0){
             if (cart[i].qty < 1 || qty === 0){
                 cart.splice(i, 1);
             }
-           
+            showItems()
             return
         }
     }
 }
+//updatecart function
+function updateCart(name, qty) {
+    for (let i = 0; i < cart.length; i++)
+        if (cart[i].name === name) {
+            if (qty < 1){
+                removeItem(name)
+                return
+            }
+            cart[i].qty = qty
+            showItems()
+            return
+        }
+}
 
 // add items to cart and display them
-addItem('Apple', 0.99);
-addItem('Orange', 1.29);
-addItem('Opinion', 0.02);
-addItem('Orange', 1.29);
-addItem('Apple', 0.99);
-addItem('Frisbee', 9.92);
-addItem('Apple', 0.99);
-showItems();
-removeItem('Frisbee');
-removeItem('Apple', 1);
-showItems();
-console.log(itemList)
+// addItem('Apple', 0.99);
+// addItem('Orange', 1.29);
+// addItem('Opinion', 0.02);
+// addItem('Orange', 1.29);
+// addItem('Apple', 0.99);
+// addItem('Frisbee', 9.92);
+// addItem('Apple', 0.99);
+// showItems();
+// removeItem('Frisbee');
+// removeItem('Apple', 1);
+// showItems();
+// console.log(itemList)
